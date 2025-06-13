b05fe7e8b3cef6b7e3bb90cb78e6023eda23cabe
wtzql <18339234993@163.com>
2025-06-13 16:27:33 +0800

    first commit

---
+++ src/test/java/cn/edu/seig/vibemusic/VibeMusicServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.seig.vibemusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeMusicServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
